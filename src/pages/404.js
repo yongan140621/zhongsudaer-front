41c65dc51521eec82b91db8a5fa7032255ba9302
karott7 <karott7@gmail.com>
2023-01-13 18:07:12 +0800

    优化项目体积

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import Router from 'next/router'
+import { useEffect } from 'react'
+
+export default function NoMatch(props) {
+  useEffect(() => {
+    Router.replace('/')
+  })
+
+  return null
+}
